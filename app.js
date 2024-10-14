79ac7c795e59408a26f8dee8d93cb11ba315426c
Seokgyun Kang <skang88@asu.edu>
2024-10-13 21:13:07 -0400

    change app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! This is second commit. And finally, I can do automation with Jenkins')
+    res.send('Hello World! This is second commit. And finally, I can do automation with Jenkins. And After the instance has stopped...')
 })
 
 app.listen(port, '0.0.0.0', () => {
