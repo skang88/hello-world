6dc130f37b6f00bb0bb4f81538033e156d285383
Seokgyun Kang <g240001@seohan.com>
2024-10-16 08:58:38 -0400

    Add dotenv

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+require('dotenv').config();
+
+console.log(process.env.DB_ENGINE);
+
+
 app.get('/', (req, res) => {
     const currentTime = new Date().toLocaleString();  // 현재 시간 추가
     const message = `
         <h1>Hello World!</h1>
+        <p>DB Engine is ${process.env.DB_ENGINE} ! </p>
         <p>This is the third commit. Jenkins automation is finally working!</p>
         <p>I chane the trigger to  Poll SCM H 20 * * * which means 8:30 PM Coordinated Universal Time</p>
         <p>Good Luck</p>
